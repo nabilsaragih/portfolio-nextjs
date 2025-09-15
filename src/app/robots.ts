b83c00d828dfe74bdde9d7cfc691aaf4492b69de
nabilsaragih <nabilsaragih33@gmail.com>
2025-09-15 08:48:45 +0700

    feat: sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,8 +3,8 @@ import { headers } from 'next/headers';
 
 export const dynamic = 'force-dynamic';
 
-export default function robots(): MetadataRoute.Robots {
-  const h = headers();
+export default async function robots(): Promise<MetadataRoute.Robots> {
+  const h = await headers();
   const host = h.get('x-forwarded-host') ?? h.get('host');
   const proto = h.get('x-forwarded-proto') ?? 'https';
   const site = host ? `${proto}://${host}` : 'http://localhost:3000';
